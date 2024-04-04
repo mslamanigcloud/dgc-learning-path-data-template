a041a6c8f7fe3b3d5279682ea4ea8fa5c0d73495
Jules Rubin <jules.rubin.jr@gmail.com>
2024-04-04 11:49:21 +0200

    pair review terraform

--- iac/cloud_functions.tf
+++ iac/cloud_functions.tf
@@ -1,29 +1,29 @@
 # Generates an archive of the source code compressed as a .zip file.
-data "archive_file" "source" {
+data "archive_file" "source_trigger_on_file" {
   type        = "zip"
   source_dir  = "../cloud_functions/cf_trigger_on_file/src"
-  output_path = "tmp/function.zip"
+  output_path = "tmp/trigger_on_file.zip"
 }
 
 # Add source code zip to the Cloud Function's bucket
-resource "google_storage_bucket_object" "zip" {
-  source       = data.archive_file.source.output_path
+resource "google_storage_bucket_object" "zip_trigger_on_file" {
+  source       = data.archive_file.source_trigger_on_file.output_path
   content_type = "application/zip"
 
   # Append to the MD5 checksum of the files's content
   # to force the zip to be updated as soon as a change occurs
-  name   = "src-${data.archive_file.source.output_md5}.zip"
+  name   = "src-${data.archive_file.source_trigger_on_file.output_md5}.zip"
   bucket = google_storage_bucket.cloud_functions_sources.name
 
   # Dependencies are automatically inferred so these lines can be deleted
   depends_on = [
     google_storage_bucket.cloud_functions_sources, # declared in `storage.tf`
-    data.archive_file.source
+    data.archive_file.source_trigger_on_file
   ]
 }
 
 # Create the Cloud function triggered by a `Finalize` event on the bucket
-resource "google_cloudfunctions_function" "function" {
+resource "google_cloudfunctions_function" "function_trigger_on_file" {
   region  = "europe-west1"
   name    = "check-file-format"
   runtime = "python311" # of course changeable
@@ -31,7 +31,7 @@ resource "google_cloudfunctions_function" "function" {
 
   # Get the source code of the cloud function as a Zip compression
   source_archive_bucket = google_storage_bucket.cloud_functions_sources.name
-  source_archive_object = google_storage_bucket_object.zip.name
+  source_archive_object = google_storage_bucket_object.zip_trigger_on_file.name
 
   # Must match the function name in the cloud function `main.py` source code
   entry_point = "check_file_format"
@@ -45,6 +45,6 @@ resource "google_cloudfunctions_function" "function" {
   # Dependencies are automatically inferred so these lines can be deleted
   depends_on = [
     google_storage_bucket.cloud_functions_sources,
-    google_storage_bucket_object.zip
+    google_storage_bucket_object.zip_trigger_on_file
   ]
 }
