7ef98f7e8d7649f38166a5aee59560fc21888ae6
Jules Rubin <jules.rubin@efrei.net>
2024-04-24 16:37:02 +0200

    feat(bq) added raw, staging and cleaned customer tables

--- iac/bigquery.tf
+++ iac/bigquery.tf
@@ -29,3 +29,27 @@ resource "google_bigquery_table" "store_cleaned" {
   schema              = file("../schemas/cleaned/store.json")
   deletion_protection = false
 }
+
+resource "google_bigquery_table" "customer_raw" {
+  dataset_id          = google_bigquery_dataset.raw.dataset_id
+  table_id            = "customer"
+  project             = var.project_id
+  schema              = file("../schemas/raw/customer.json")
+  deletion_protection = false
+}
+
+resource "google_bigquery_table" "customer_staging" {
+  dataset_id          = google_bigquery_dataset.cleaned.dataset_id
+  table_id            = "customer_staging"
+  project             = var.project_id
+  schema              = file("../schemas/cleaned/customer.json")
+  deletion_protection = false
+}
+
+resource "google_bigquery_table" "customer_cleaned" {
+  dataset_id          = google_bigquery_dataset.cleaned.dataset_id
+  table_id            = "customer"
+  project             = var.project_id
+  schema              = file("../schemas/cleaned/customer.json")
+  deletion_protection = false
+}
